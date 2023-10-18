29e4d277f5a632342bb8e35b65b6fcf10e1721c6
Hüseyin Doğdu <66470348+huso987@users.noreply.github.com>
2023-10-18 11:35:40 +0300

    Login ve register html eklendi

--- chatpot/chatbot/views.py
+++ chatpot/chatbot/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import openai
 
+
+openai_api_key = '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'
+openai.api_key = openai_api_key
+
+def ask_openai(message):
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=message,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+    answer = response.choices[0].text.strip()
+    return answer
 # Create your views here.
 def chatbot(request):
     if request.method == 'POST':
         message = request.POST.get('message')
-        response = 'Bu benim cevabım'
+        response = ask_openai(message)
         return JsonResponse({ 'message': message, 'response': response})
 
     return render(request, 'chatbot.html')
